74aa50285bb3010af8fccfb2bacde35fc531342d
Suraj Kumar Jana <surajjana2@gmail.com>
2016-12-16 14:32:38 +0530

    new commit

--- index.js
+++ index.js
@@ -112,6 +112,10 @@ function eventHandle(sender, event){
 					var time_stamp = Date.now()
 
 					sendMessage(sender, msgData, sent_msg, received_msg, msg_cat, time_stamp)
+				}else if(text.toLowerCase == 'my to-do'){
+					var cursor = db.collection('fb_msg_log').find({ "fb_id": parseInt(sender), "msg_cat": "to-do" }).toArray(function(err, res){
+						console.log(res)
+					})
 				}else if(text.toLowerCase() == 'suggest me some hoodies'){
 					var msgData = {
 					  "attachment": {
