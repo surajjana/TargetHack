48f2488702423b791c4e4b08516c1f017dcd5be5
Suraj Kumar Jana <surajjana2@gmail.com>
2016-12-16 11:15:10 +0530

    new commit

--- index.js
+++ index.js
@@ -102,11 +102,115 @@ function eventHandle(sender, event){
 			}else if (event.message && event.message.text) {
 				var text = event.message.text
 
-				if(text.toLowerCase() == 'get me some grocery'){
-					var msgData = { text: 'Let me look for it'}
+				if(text.toLowerCase() == 'suggest me some t-shirts'){
+					var msgData = {
+					    "attachment": {
+					        "type": "template",
+					        "payload": {
+					            "template_type": "list",
+					            "elements": [
+					                {
+					                    "title": "Classic T-Shirt Collection",
+					                    "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+					                    "subtitle": "See all our colors",
+					                    "default_action": {
+					                        "type": "web_url",
+					                        "url": "https://peterssendreceiveapp.ngrok.io/shop_collection",
+					                        "messenger_extensions": true,
+					                        "webview_height_ratio": "tall",
+					                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+					                    },
+					                    "buttons": [
+					                        {
+					                            "title": "View",
+					                            "type": "web_url",
+					                            "url": "https://peterssendreceiveapp.ngrok.io/collection",
+					                            "messenger_extensions": true,
+					                            "webview_height_ratio": "tall",
+					                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"                        
+					                        }
+					                    ]
+					                },
+					                {
+					                    "title": "Classic White T-Shirt",
+					                    "image_url": "https://peterssendreceiveapp.ngrok.io/img/white-t-shirt.png",
+					                    "subtitle": "100% Cotton, 200% Comfortable",
+					                    "default_action": {
+					                        "type": "web_url",
+					                        "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+					                        "messenger_extensions": true,
+					                        "webview_height_ratio": "tall",
+					                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+					                    },
+					                    "buttons": [
+					                        {
+					                            "title": "Shop Now",
+					                            "type": "web_url",
+					                            "url": "https://peterssendreceiveapp.ngrok.io/shop?item=100",
+					                            "messenger_extensions": true,
+					                            "webview_height_ratio": "tall",
+					                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"                        
+					                        }
+					                    ]                
+					                },
+					                {
+					                    "title": "Classic Blue T-Shirt",
+					                    "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+					                    "subtitle": "100% Cotton, 200% Comfortable",
+					                    "default_action": {
+					                        "type": "web_url",
+					                        "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+					                        "messenger_extensions": true,
+					                        "webview_height_ratio": "tall",
+					                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+					                    },
+					                    "buttons": [
+					                        {
+					                            "title": "Shop Now",
+					                            "type": "web_url",
+					                            "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+					                            "messenger_extensions": true,
+					                            "webview_height_ratio": "tall",
+					                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"                        
+					                        }
+					                    ]                
+					                },
+					                {
+					                    "title": "Classic Black T-Shirt",
+					                    "image_url": "https://peterssendreceiveapp.ngrok.io/img/black-t-shirt.png",
+					                    "subtitle": "100% Cotton, 200% Comfortable",
+					                    "default_action": {
+					                        "type": "web_url",
+					                        "url": "https://peterssendreceiveapp.ngrok.io/view?item=102",
+					                        "messenger_extensions": true,
+					                        "webview_height_ratio": "tall",
+					                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+					                    },
+					                    "buttons": [
+					                        {
+					                            "title": "Shop Now",
+					                            "type": "web_url",
+					                            "url": "https://peterssendreceiveapp.ngrok.io/shop?item=102",
+					                            "messenger_extensions": true,
+					                            "webview_height_ratio": "tall",
+					                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"                        
+					                        }
+					                    ]                
+					                }
+					            ],
+					             "buttons": [
+					                {
+					                    "title": "View More",
+					                    "type": "postback",
+					                    "payload": "payload"                        
+					                }
+					            ]  
+					        }
+					    }
+					}
 					var sent_msg = text
-					var received_msg = msgData.text
-					var msg_cat = 'grocery'
+					var received_msg = 'list'
+					var msg_cat = 'test'
 					var time_stamp = Date.now()
 
 					sendMessage(sender, msgData, sent_msg, received_msg, msg_cat, time_stamp)
