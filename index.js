a5f0eeef9b09406204be56172f06681a0cddf31a
Suraj Kumar Jana <surajjana2@gmail.com>
2016-12-16 11:23:06 +0530

    new commit

--- index.js
+++ index.js
@@ -116,85 +116,19 @@ function eventHandle(sender, event){
 					                    "subtitle": "100% Cotton, 200% Comfortable",
 					                    "default_action": {
 					                        "type": "web_url",
-					                        "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+					                        "url": "",
 					                        "messenger_extensions": true,
 					                        "webview_height_ratio": "tall",
-					                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+					                        "fallback_url": ""
 					                    },
 					                    "buttons": [
 					                        {
 					                            "title": "Buy",
 					                            "type": "web_url",
-					                            "url": "https://peterssendreceiveapp.ngrok.io/shop?item=100",
+					                            "url": "",
 					                            "messenger_extensions": true,
 					                            "webview_height_ratio": "tall",
-					                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"                        
-					                        }
-					                    ]                
-					                },
-					                {
-					                    "title": "Classic Blue T-Shirt",
-					                    "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
-					                    "subtitle": "100% Cotton, 200% Comfortable",
-					                    "default_action": {
-					                        "type": "web_url",
-					                        "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
-					                        "messenger_extensions": true,
-					                        "webview_height_ratio": "tall",
-					                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-					                    },
-					                    "buttons": [
-					                        {
-					                            "title": "Buy",
-					                            "type": "web_url",
-					                            "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
-					                            "messenger_extensions": true,
-					                            "webview_height_ratio": "tall",
-					                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"                        
-					                        }
-					                    ]                
-					                },
-					                {
-					                    "title": "Classic Black T-Shirt",
-					                    "image_url": "https://peterssendreceiveapp.ngrok.io/img/black-t-shirt.png",
-					                    "subtitle": "100% Cotton, 200% Comfortable",
-					                    "default_action": {
-					                        "type": "web_url",
-					                        "url": "https://peterssendreceiveapp.ngrok.io/view?item=102",
-					                        "messenger_extensions": true,
-					                        "webview_height_ratio": "tall",
-					                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-					                    },
-					                    "buttons": [
-					                        {
-					                            "title": "Buy",
-					                            "type": "web_url",
-					                            "url": "https://peterssendreceiveapp.ngrok.io/shop?item=102",
-					                            "messenger_extensions": true,
-					                            "webview_height_ratio": "tall",
-					                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"                        
-					                        }
-					                    ]                
-					                },
-					                {
-					                    "title": "Classic Gray T-Shirt",
-					                    "image_url": "https://peterssendreceiveapp.ngrok.io/img/gray-t-shirt.png",
-					                    "subtitle": "100% Cotton, 200% Comfortable",
-					                    "default_action": {
-					                        "type": "web_url",
-					                        "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
-					                        "messenger_extensions": true,
-					                        "webview_height_ratio": "tall",
-					                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-					                    },
-					                    "buttons": [
-					                        {
-					                            "title": "Buy",
-					                            "type": "web_url",
-					                            "url": "https://peterssendreceiveapp.ngrok.io/shop?item=103",
-					                            "messenger_extensions": true,
-					                            "webview_height_ratio": "tall",
-					                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"                        
+					                            "fallback_url": ""                        
 					                        }
 					                    ]                
 					                }
